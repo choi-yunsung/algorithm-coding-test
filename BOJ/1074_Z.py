ae3e99fb3e785f01b00216b4e2cb18c5439985af
Yunsung Choi <jws02025@gmail.com>
2021-09-16 09:25:22 +0900

    Update 1074_Z.py

--- BOJ/1074_Z.py
+++ BOJ/1074_Z.py
@@ -5,13 +5,13 @@ def dfs(i, j, topleft, l):
         return
     else:
         l = l//2
-        if r < i+l and c < j+l:
+        if r < i+l and c < j+l: #2사분면
             dfs(i, j, topleft, l)
-        if r < i+l and c >= j+l:
+        if r < i+l and c >= j+l: #1사분면
             dfs(i, j+l, topleft+l**2, l)
-        if r >= i+l and c < j+l:
+        if r >= i+l and c < j+l: #3사분면
             dfs(i+l, j, topleft+l**2*2 ,l)
-        if r >= i+l and c >= j+l:
+        if r >= i+l and c >= j+l: #4사분면
             dfs(i+l, j+l, topleft+l**2*3, l)
 
 n,r,c = map(int, input().split())
