6743b5a91b030afba575131934a3a33f16611beb
Yunsung Choi <jws02025@gmail.com>
2021-09-18 14:55:24 +0900

    Update 2667_단지번호붙이기.py

--- BOJ/2667_단지번호붙이기.py
+++ BOJ/2667_단지번호붙이기.py
@@ -2,9 +2,7 @@ import sys
 from collections import deque
 
 N = int(sys.stdin.readline().rstrip())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
